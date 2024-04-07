55ff782b1ac51faae20bc8cdc1c8d999cb8e6f42
dafg05 <danialefloresg@gmail.com>
2024-04-06 23:10:26 -0400

    analysis coding

---
+++ evaluation/analysis.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+from typing import List
+import pandas as pd
+import numpy as np
+
+TRAIN_RUNS_DIR = Path("train_runs")
+ORIGIN_VECTOR = np.array([0, 1]) # [kl_divergence, overlapping_area]
+
+def analysis(eval_runs_paths: List[Path], report_path: Path, n: int):
+    best_models_dict = get_best_models(eval_runs_paths, n)
+    analysis_data = {
+        "model_name": [],
+        "validation_loss": [],
+        "num_transformations": [],
+        "num_replacements": [],
+        "out_of_style_prob": [],
+    }
+
+    report_df = pd.read_csv(report_path, index_col="training_start_time")
+    
+    for eval_run_path in best_models_dict.keys():
+        model_name = get_model_name_from_eval_run(eval_run_path)
+        analysis_data["model_name"].append(model_name)
+        analysis_data["validation_loss"].append(best_models_dict[eval_run_path])
+
+        training_start_time = int(model_name.split("_")[-1])
+        num_transformations = report_df.loc[training_start_time, "num_transformations"]
+        num_replacements = report_df.loc[training_start_time, "num_replacements"]
+        out_of_style_prob = report_df.loc[training_start_time, "out_of_style_prob"]
+
+        analysis_data["num_transformations"].append(num_transformations)
+        analysis_data["num_replacements"].append(num_replacements)
+        analysis_data["out_of_style_prob"].append(out_of_style_prob)
+    
+    return analysis_data
+
+def get_best_models(eval_runs_paths, n):
+    model_validation_losses = {}
+    for eval_run_path in eval_runs_paths:
+        model_score = get_validation_loss(eval_run_path)
+        model_validation_losses[eval_run_path] = model_score
+    return get_min_loss_entries(model_validation_losses, n)
+    
+def get_min_loss_entries(losses_dict, n):
+    # Sort the dictionary by its values (ascending order) and select the first n items
+    min_loss_entries = sorted(losses_dict.items(), key=lambda item: item[1])[:n]
+    # Convert the sorted list of tuples back to a dictionary
+    return dict(min_loss_entries)
+
+def get_validation_loss(eval_run_path):
+    eval_results = pd.read_csv(eval_run_path / "results.csv", index_col="feature")
+    validation_loss = 0
+    # iterate over the rows of the dataframe
+    for _, row in eval_results.iterrows():
+        # our metrics vector is going to be [kl_divergence, overlapping_area]
+        # retrieve these vales from the dataframe
+        metrics_vector = np.array([row["kl_divergence"], row["overlapping_area"]])
+        # calculate the euclidean distance between the metrics vector and the origin vector
+        distance = np.linalg.norm(metrics_vector - ORIGIN_VECTOR)
+        # add the distance to the validation loss
+        validation_loss += distance
+    return validation_loss
+
+def get_model_name_from_eval_run(eval_run_path):
+    # TEST ME FIRST
+    run_name = eval_run_path.stem
+
+    return "_".join(run_name.split("_")[2:])
---
+++ tests/testAnalysis.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+import pandas as pd
+
+from .constants import TEST_DATA_DIR
+
+# from ..evaluation import analysis
+from ..evaluation import analysis
+
+EVAL_RUN_PATH_1 = TEST_DATA_DIR / "eval_runs" / "evaluation_1712200441_full_solar-shadow_1711253355"
+EVAL_RUN_PATH_2 = TEST_DATA_DIR / "eval_runs" / "evaluation_1712202255_full_solar-shadow_1711265770"
+EVAL_RUN_PATH_3 = TEST_DATA_DIR / "eval_runs" / "evaluation_1712202211_full_solar-shadow_1711277715"
+EVAL_RUN_PATHS = [EVAL_RUN_PATH_1, EVAL_RUN_PATH_2, EVAL_RUN_PATH_3]
+REPORT_PATH = TEST_DATA_DIR / "wandb_training_runs_report.csv"
+
+OUT_DIR = TEST_DATA_DIR / "analysis_out"
+
+LOSSES_DICT = {
+    "model1": 0.1,
+    "model2": 0.2,
+    "model3": 0.3,
+    "model4": 0.4,
+}
+
+def test_get_min_loss_entries():
+    n = 2
+    expected = {
+        "model1": 0.1,
+        "model2": 0.2,
+    }
+    result = analysis.get_min_loss_entries(LOSSES_DICT, n)
+    assert result == expected
+    print(f"Test get_min_loss_entries passed")
+
+def test_get_model_name_from_eval_run():
+    eval_run_path = EVAL_RUN_PATH_1
+    model_name = analysis.get_model_name_from_eval_run(eval_run_path)
+    assert model_name == "full_solar-shadow_1711253355", f'Got: {model_name}'
+    print(f"Test get_model_name_from_eval_run passed")
+
+def test_analysis():
+    n = 2
+    analysis_data = analysis.analysis(EVAL_RUN_PATHS, REPORT_PATH, n)
+    analysis_df = pd.DataFrame(analysis_data)
+    assert analysis_df.shape[0] == n, f"Expected {n} rows, got {analysis_df.shape[0]}"
+    # write the dataframe to a csv file
+    analysis_df.to_csv(OUT_DIR / "analysis.csv")
+    print(f"Analysis data written to csv file at {OUT_DIR / 'analysis.csv'}")
+
+if __name__ == "__main__":
+    test_get_min_loss_entries()
+    test_get_model_name_from_eval_run()
+    test_analysis()
+    
+    
+    
